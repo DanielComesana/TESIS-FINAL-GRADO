4297be45d03f08b7de1b1b37033def2299e3619a
DanielComesana <107628528+DanielComesana@users.noreply.github.com>
2022-09-04 12:05:43 +0200

    Trabajo Final de Grado
    Detección de plásticos PET por inteligencia artificial

---
+++ Mask RCNNN Personal/prueba.py
@@ -0,0 +1,187 @@
+import os
+import sys
+import json
+import datetime
+import numpy as np
+import skimage.draw
+import cv2
+from mrcnn.visualize import display_instances
+import matplotlib.pyplot as plt
+
+# Root directory of the project
+ROOT_DIR = "C:/Users/DANIEL/Documents/MASKCOPIA"
+
+# Import Mask RCNN
+sys.path.append(ROOT_DIR)  # To find local version of the library
+from mrcnn.config import Config
+from mrcnn import model as modellib, utils
+
+# Path to trained weights file
+COCO_WEIGHTS_PATH = os.path.join(ROOT_DIR, "mask_rcnn_coco.h5")
+
+# Directory to save logs and model checkpoints, if not provided
+# through the command line argument --logs
+DEFAULT_LOGS_DIR = os.path.join(ROOT_DIR, "logs")
+
+
+
+class CustomConfig(Config):
+    """Configuration for training on the custom  dataset.
+    Derives from the base Config class and overrides some values.
+    """
+    # Give the configuration a recognizable name
+    NAME = "object"
+
+    # We use a GPU with 12GB memory, which can fit two images.
+    # Adjust down if you use a smaller GPU.
+    IMAGES_PER_GPU = 1
+
+    # Number of classes (including background)
+    NUM_CLASSES = 1 + 1  # Background + phone,laptop and mobile
+
+    # Number of training steps per epoch
+    STEPS_PER_EPOCH = 10
+
+    # Skip detections with < 90% confidence
+    DETECTION_MIN_CONFIDENCE = 0.9
+
+    LEARNING_RATE=0.001
+
+############################################################
+#  Dataset
+############################################################
+
+class CustomDataset(utils.Dataset):
+
+    def load_custom(self, dataset_dir, subset):
+        """Load a subset of the Bottle dataset.
+        dataset_dir: Root directory of the dataset.
+        subset: Subset to load: train or val
+        """
+        # Add classes. We have only one class to add.
+        self.add_class("bottle", 1, "bottle")
+
+        # Train or validation dataset?
+        assert subset in ["train", "val"]
+        dataset_dir = os.path.join(dataset_dir, subset)
+
+        # Load annotations
+        # VGG Image Annotator (up to version 1.6) saves each image in the form:
+        # { 'filename': '28503151_5b5b7ec140_b.jpg',
+        #   'regions': {
+        #       '0': {
+        #           'region_attributes': {},
+        #           'shape_attributes': {
+        #               'all_points_x': [...],
+        #               'all_points_y': [...],
+        #               'name': 'polygon'}},
+        #       ... more regions ...
+        #   },
+        #   'size': 100202
+        # }
+        # We mostly care about the x and y coordinates of each region
+        # Note: In VIA 2.0, regions was changed from a dict to a list.
+        annotations = json.load(open(dataset_dir + '/test.json'))
+        annotations = list(annotations.values())  # don't need the dict keys
+
+        # The VIA tool saves images in the JSON even if they don't have any
+        # annotations. Skip unannotated images.
+        annotations = [a for a in annotations if a['regions']]
+
+        # Add images
+        for a in annotations:
+            # Get the x, y coordinaets of points of the polygons that make up
+            # the outline of each object instance. These are stores in the
+            # shape_attributes (see json format above)
+            # The if condition is needed to support VIA versions 1.x and 2.x.
+            if type(a['regions']) is dict:
+                polygons = [r['shape_attributes'] for r in a['regions'].values()]
+            else:
+                polygons = [r['shape_attributes'] for r in a['regions']] 
+
+
+            # load_mask() needs the image size to convert polygons to masks.
+            # Unfortunately, VIA doesn't include it in JSON, so we must read
+            # the image. This is only managable since the dataset is tiny.
+            image_path = os.path.join(dataset_dir, a['filename'])
+            #print("Image_Path >", image_path)
+            image = skimage.io.imread(image_path)
+            height, width = image.shape[:2]
+            self.add_image(
+                "bottle",
+                image_id=a['filename'],  # use file name as a unique image id
+                path=image_path,
+                width=width, height=height,
+                polygons=polygons)
+
+    def load_mask(self, image_id):
+        """Generate instance masks for an image.
+       Returns:
+        masks: A bool array of shape [height, width, instance count] with
+            one mask per instance.
+        class_ids: a 1D array of class IDs of the instance masks.
+        """
+        # If not a bottle dataset image, delegate to parent class.
+        image_info = self.image_info[image_id]
+        if image_info["source"] != "bottle":
+            return super(self.__class__, self).load_mask(image_id)
+
+        # Convert polygons to a bitmap mask of shape
+        # [height, width, instance_count]
+        info = self.image_info[image_id]
+        mask = np.zeros([info["height"], info["width"], len(info["polygons"])],
+                        dtype=np.uint8)
+        for i, p in enumerate(info["polygons"]):
+            # Get indexes of pixels inside the polygon and set them to 1
+            rr, cc = skimage.draw.polygon(p['all_points_y'], p['all_points_x'])
+            mask[rr, cc, i] = 1
+
+        # Return mask, and array of class IDs of each instance. Since we have
+        # one class ID only, we return an array of 1s
+        return mask.astype(bool), np.ones([mask.shape[-1]], dtype=np.int32)
+
+    def image_reference(self, image_id):
+        """Return the path of the image."""
+        info = self.image_info[image_id]
+        if info["source"] == "bottle":
+            return info["path"]
+        else:
+            super(self.__class__, self).image_reference(image_id)
+def train(model):
+    """Train the model."""
+    # Training dataset.
+    dataset_train = CustomDataset()
+    dataset_train.load_custom("C:/Users/DANIEL/Documents/MASKCOPIA/DATASET", "train")
+    dataset_train.prepare()
+
+    # Validation dataset
+    dataset_val = CustomDataset()
+    dataset_val.load_custom("C:/Users/DANIEL/Documents/MASKCOPIA/DATASET", "val")
+    dataset_val.prepare()
+
+    # *** This training schedule is an example. Update to your needs ***
+    # Since we're using a very small dataset, and starting from
+    # COCO trained weights, we don't need to train too long. Also,
+    # no need to train all layers, just the heads should do it.
+    print("Training network heads")
+    model.train(dataset_train, dataset_val,
+                learning_rate=config.LEARNING_RATE,
+                epochs=20,
+                layers='heads')
+				
+				
+				
+config = CustomConfig()
+model = modellib.MaskRCNN(mode="training", config=config,
+                                  model_dir=DEFAULT_LOGS_DIR)
+
+weights_path = COCO_WEIGHTS_PATH
+        # Download weights file
+if not os.path.exists(weights_path):
+  utils.download_trained_weights(weights_path)
+
+model.load_weights(weights_path, by_name=True, exclude=[
+            "mrcnn_class_logits", "mrcnn_bbox_fc",
+            "mrcnn_bbox", "mrcnn_mask"])
+
+train(model)			
